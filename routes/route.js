f68327310ab28d2ce1f298fc35b917684f0115a3
osaro123 <chibueze.k.muoneke@gmail.com>
2024-07-02 19:09:31 +0100

    first commit

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,6 @@ const express = require("express")
 const getLocationAndWeatherController = require("../controllers/controller")
 const router = express.Router()
 
-router.get("/api/hello", getLocationAndWeatherController)
+router.get("/hello", getLocationAndWeatherController)
 
 module.exports = router
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.use("/", routes)
+app.use("/api", routes)
+
+app.get("/", () => console.log(`hello world`))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
